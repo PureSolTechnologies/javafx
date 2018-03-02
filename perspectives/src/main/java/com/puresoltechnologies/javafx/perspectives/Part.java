f9f3ed0af10567bcba37107de96e02f1801ac5b6
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2018-03-02 22:41:56 +0700

    Work on perspectives.

--- perspectives/src/main/java/com/puresoltechnologies/javafx/perspectives/Part.java
+++ perspectives/src/main/java/com/puresoltechnologies/javafx/perspectives/Part.java
@@ -1,8 +1,12 @@
 package com.puresoltechnologies.javafx.perspectives;
 
-public interface Part extends PerspectiveElement {
+public interface Part extends PerspectiveElement, AutoCloseable {
 
     public String getName();
 
     public boolean isSingleton();
+
+    @Override
+    public void close();
+
 }
