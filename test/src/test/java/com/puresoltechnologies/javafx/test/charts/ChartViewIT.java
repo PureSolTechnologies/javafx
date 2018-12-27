8cdc23d724109e73a1a6340d46daa88b28ac2a9c
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2018-12-27 23:38:17 +0100

    Work on test.

--- test/src/test/java/com/puresoltechnologies/javafx/test/charts/ChartViewIT.java
+++ test/src/test/java/com/puresoltechnologies/javafx/test/charts/ChartViewIT.java
@@ -1,11 +1,16 @@
 package com.puresoltechnologies.javafx.test.charts;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 
 import com.puresoltechnologies.javafx.charts.ChartView;
+import com.puresoltechnologies.javafx.preferences.Preferences;
 
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,6 +19,16 @@ import javafx.stage.Stage;
 public class ChartViewIT extends ApplicationTest {
 
     ChartView chartView;
+
+    @BeforeAll
+    public static void initialize() throws IOException {
+	Preferences.initialize();
+    }
+
+    @AfterAll
+    public static void shutdown() {
+	Preferences.shutdown();
+    }
 
     @Override
     public void start(Stage stage) {
