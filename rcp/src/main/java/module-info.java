196dff78b2f307b81536249e83b2c59f518749e3
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2019-07-01 23:00:19 +0200

    Fixes for builds.

---
+++ rcp/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.puresoltechnologies.javafx.rcp {
+    requires transitive com.puresoltechnologies.javafx.extensions;
+    requires transitive com.puresoltechnologies.javafx.perspectives;
+    requires transitive com.puresoltechnologies.javafx.preferences;
+    requires transitive com.puresoltechnologies.javafx.reactive;
+    requires transitive com.puresoltechnologies.javafx.services;
+    requires transitive com.puresoltechnologies.javafx.tasks;
+    requires transitive com.puresoltechnologies.javafx.utils;
+    requires transitive com.puresoltechnologies.javafx.workspaces;
+}
