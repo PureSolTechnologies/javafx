ba2e7d1d8f2bce9b9749e426ca5084c39d405db7
Rick-Rainer Ludwig <rick-rainer.ludwig@senorics.com>
2019-07-16 16:19:17 +0200

    Fix in module-info.

--- reactive/src/main/java/module-info.java
+++ reactive/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.puresoltechnologies.javafx.reactive {
     requires slf4j.api;
 
     exports com.puresoltechnologies.javafx.reactive;
+    exports com.puresoltechnologies.javafx.reactive.flux;
 
     uses com.puresoltechnologies.javafx.reactive.flux.Store;
 }
