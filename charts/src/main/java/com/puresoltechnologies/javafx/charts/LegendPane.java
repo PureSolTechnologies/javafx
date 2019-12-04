3f3ea9b88a1fb5f824eccda122edead339e42d8f
Rick-Rainer Ludwig <rick-rainer.ludwig@senorics.com>
2019-12-04 09:48:53 +0100

    Fixed interactive update of plot titles in chart legend.

--- charts/src/main/java/com/puresoltechnologies/javafx/charts/LegendPane.java
+++ charts/src/main/java/com/puresoltechnologies/javafx/charts/LegendPane.java
@@ -21,44 +21,45 @@ public class LegendPane extends TilePane {
     private final ChartView chartView;
 
     public LegendPane(ChartView chartView, ObservableList<Plot<?, ?, ?>> plots) {
-	this.chartView = chartView;
-	setPadding(new Insets(5.0));
-	setHgap(5.0);
-	setVgap(5.0);
-	plots.addListener((ListChangeListener<Plot<?, ?, ?>>) (change) -> {
-	    showSymbols(change.getList());
-	});
-	chartView.backgroundColorProperty().addListener((observable, oldValue, newValue) -> {
-	    updateBackground();
-	});
-	updateBackground();
+        this.chartView = chartView;
+        setPadding(new Insets(5.0));
+        setHgap(5.0);
+        setVgap(5.0);
+        plots.addListener((ListChangeListener<Plot<?, ?, ?>>) (change) -> {
+            showSymbols(change.getList());
+        });
+        chartView.backgroundColorProperty().addListener((observable, oldValue, newValue) -> {
+            updateBackground();
+        });
+        updateBackground();
     }
 
     private void updateBackground() {
-	setBackground(
-		new Background(new BackgroundFill(chartView.getBackgroundColor(), CornerRadii.EMPTY, Insets.EMPTY)));
+        setBackground(
+                new Background(new BackgroundFill(chartView.getBackgroundColor(), CornerRadii.EMPTY, Insets.EMPTY)));
     }
 
     private void showSymbols(ObservableList<? extends Plot<?, ?, ?>> plots) {
-	getChildren().clear();
-	plots.forEach(plot -> showSymbol(plot));
+        getChildren().clear();
+        plots.forEach(plot -> showSymbol(plot));
     }
 
     private void showSymbol(Plot<?, ?, ?> plot) {
-	GridPane pane = new GridPane();
+        GridPane pane = new GridPane();
 
-	Pane symbolPane = new Pane();
-	MarkerCanvas canvas = new MarkerCanvas(symbolPane, plot);
-	symbolPane.getChildren().add(canvas);
-	symbolPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        Pane symbolPane = new Pane();
+        MarkerCanvas canvas = new MarkerCanvas(symbolPane, plot);
+        symbolPane.getChildren().add(canvas);
+        symbolPane.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
-	Label label = new Label(plot.getTitle());
+        Label label = new Label();
+        label.textProperty().bind(plot.titleProperty());
 
-	GridPane.setConstraints(symbolPane, 0, 0, 1, 1, HPos.LEFT, VPos.CENTER, Priority.SOMETIMES, Priority.NEVER);
-	GridPane.setConstraints(label, 1, 0, 1, 1, HPos.LEFT, VPos.CENTER, Priority.SOMETIMES, Priority.NEVER);
-	pane.getChildren().addAll(symbolPane, label);
+        GridPane.setConstraints(symbolPane, 0, 0, 1, 1, HPos.LEFT, VPos.CENTER, Priority.SOMETIMES, Priority.NEVER);
+        GridPane.setConstraints(label, 1, 0, 1, 1, HPos.LEFT, VPos.CENTER, Priority.SOMETIMES, Priority.NEVER);
+        pane.getChildren().addAll(symbolPane, label);
 
-	getChildren().add(pane);
+        getChildren().add(pane);
     }
 
 }
